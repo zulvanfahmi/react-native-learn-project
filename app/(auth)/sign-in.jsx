c3987a44a3c90487c748feb9b387414eb40f2c7d
Zulvan Fahmi <zulvanfahmi1612@gmail.com>
2025-02-06 10:47:21 +0700

    update 06022025

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -16,24 +16,32 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh]px-4 my-6">
-          <Image 
-          source={images.logo}
-          resizeMode='contain'
-          className = 'w-[115px] h-[35px]'
-           />
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
 
-           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
-           </Text>
+          </Text>
 
-           <FormField
+          <FormField
             title="Email"
             value={form.email}
-            handleChangeText={(e) => setform({ ...form, email:e })}
+            handleChangeText={(e) => setform({ ...form, email: e })}
             otherStyles="mt-7"
             keyboardType="email-address"
-           />
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setform({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
         </View>
       </ScrollView>
     </SafeAreaView>
